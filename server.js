ce098d6a1cc9ded6655684595abda5f5fb4a6998
alikhaled17 <alikaled1x@gmail.com>
2023-11-20 13:09:01 +0200

    handling error middleware

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const morgan = require("morgan");
 dotenv.config({ path: "config.env" });
 const dbConnection = require("./config/db");
 const categoryRoute = require("./routes/category-route");
+const ApiError = require("./utils/ApiError");
 
 // Connect with db
 dbConnection();
@@ -22,6 +23,26 @@ if (process.env.NODE_ENV === "development") {
 
 // Mount Routes
 app.use(`/api/${process.env.VERSION}/categories`, categoryRoute);
+
+app.use("*", (req, res, next) => {
+  // Create error and send it to global handling error middleware
+  next(new ApiError(`Can't find this route ${req.originalUrl}`, 400));
+});
+
+// Global error handling middleware
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  res
+    .status(err.statusCode)
+    .json({
+      status: err.status,
+      error: err,
+      message: err.message,
+      stack: err.stack,
+    });
+});
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
---
+++ utils/ApiError.js
@@ -0,0 +1,11 @@
+// @desc   This Class is responsaple about operation errors (errors that i can predict)
+class ApiError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith("4") ? "fail" : "error";
+    this.isOperational = true;
+  }
+}
+
+module.exports = ApiError;
